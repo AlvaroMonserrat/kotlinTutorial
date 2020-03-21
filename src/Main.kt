f6250cec9bb1096288ed0efe998251334a744935
Alvaro <alvaro.monserrat@gmail.com>
2020-03-20 22:42:58 -0300

    ListOf

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,13 @@
 
 //Kotlin
+import kotlin.collections.*
 
+fun opera(lista: List<Int>, valor: Int, funcion:(Int, Int)->Int): List<Int> {
+	val result = arrayListOf<Int>()
+	for(item in lista)
+		result.add(funcion(item, valor))
+	return result
+}
 
  fun main(args: Array<String>){
 	 
@@ -10,7 +17,7 @@
 	 var max = if (a > b) a else b
 	 
 	 println(max)
-	 
+
 	 when (max){
 		 1 -> print("Maximo igual a 1")
 		 2,3 -> print("Maximo 2 o 3")
@@ -28,11 +35,20 @@
 	print(i) 
 	print(s + " World\n")
 	println(sum(6,6))
+	println("END")
 	println(double(10))
+	
+	var nums = listOf(1, 2, 3, 4)
+	for(j in nums)
+	{
+		println(j)
+	}
+	 
 }
 
 fun sum(a: Int, b: Int): Int{
 	return a+b
 }
 	
-fun double(x: Int) = 2*x 
+fun double(x: Int) = 2*x
+
